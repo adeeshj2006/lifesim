f28c89fd9f2042cc984ca5aff8e7e983cd7ed1ae
Adeesh Jain <adeesh@adeesh.com>
2025-12-07 21:47:05 +0530

    [INFO] project init

---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+    The simulation loop
+    -   Initialize world gird
+    -   List of all organisms
+    -   For timestep in range:
+        -   For each organism:
+            -   Read sensors
+            -   Run brain
+            -   Apply actions
+            -   Run world diffusion and reactions
+            -   Update energies
+            -   Visualize, print, debug
+"""
+
